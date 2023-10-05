e90f943382d7e928c08cb4867c4783967bd01881
David Reina <davidereina@hotmail.com>
2023-10-05 14:46:05 -0500

    Example of how to convert string type into int and float types

---
+++ Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float floatResult = float.Parse(stringForFloat);
+            int intResult = Int32.Parse(stringForInt);
+
+            Console.WriteLine("float result: " + floatResult);
+            Console.WriteLine("integer result: " + intResult);
+            Console.Read();
+        }
+    }
+}
